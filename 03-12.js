eab2499f31f5b313c81d88e1e714a9889a8c13e9
Mateusz Bogolubow <mateusz@bogolubow.com>
2023-11-08 14:02:44 +0100

    init 03-12

---
+++ 03-12.js
@@ -0,0 +1,16 @@
+const checker = {
+    // color: 'white',
+    player: 'Anna',
+    moves: [],
+}
+
+Object.defineProperty(checker, 'color',
+    {
+        value: 'white',
+        writable: true,
+        enumerable: true,
+        configurable: true,
+    },
+);
+
+console.log(checker);

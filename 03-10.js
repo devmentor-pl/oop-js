0b04262d7e06cb29e82893e23d404abaeac6d105
Mateusz Bogolubow <mateusz@bogolubow.com>
2023-11-09 17:16:04 +0100

    fix var name

--- 03-10.js
+++ 03-10.js
@@ -7,7 +7,7 @@ function Checker(color, player) {
 const checker = new Checker('white', 'Anna');
 
 for (const propName in checker) {
-    console.log(`${propName} => ${pawn[propName]}`);
+    console.log(`${propName} => ${checker[propName]}`);
 }
 
 // for (const value of checker) {

2b01358a33f501c3da2686316221627cf0085ef4
Mateusz Bogolubow <mateusz@bogolubow.com>
2023-11-15 07:46:02 +0100

    init 07-02

--- 07-02.js
+++ 07-02.js
@@ -1,6 +1,7 @@
 import Board from './Board.js';
 
 const board = new Board();
-board.fieldsList[1][2] = 'test';
+// board.#fieldsList[1][2] = 'test';
+board.setField('12', 'ok')
 console.log(board.getField('12'));
 console.log(board);
--- Board.js
+++ Board.js
@@ -1,5 +1,5 @@
 export default class Board {
-    fieldsList = [
+    #fieldsList = [
         ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09'],
         ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19'],
         ['20', '21', '22', '23', '24', '25', '26', '27', '28', '29'],
@@ -12,6 +12,7 @@ export default class Board {
         ['90', '91', '92', '93', '94', '95', '96', '97', '98', '99'],
     ];
 
+
     getField = function (coords) {
         if (!this.isCorrectCoords(coords)) {
             throw new Error('Incorrect coords!');
@@ -19,7 +20,7 @@ export default class Board {
 
         const [rowIndex, colIndex] = coords;
 
-        return this.fieldsList[rowIndex][colIndex];
+        return this.#fieldsList[rowIndex][colIndex];
     }
 
     setField = function (coords, value) {
@@ -27,12 +28,39 @@ export default class Board {
             throw new Error('Incorrect coords!');
         }
 
-        const [rowIndex, colIndex] = coords; 
+        // const [rowIndex, colIndex] = coords; 
+        const rowIndex = this.#getRowIndex(coords);
+        const colIndex = this.#getColIndex(coords);
 
-        this.fieldsList[rowIndex][colIndex] = value;
+        this.#fieldsList[rowIndex][colIndex] = value;
     }
 
     isCorrectCoords = function (coords) {
         return /^[0-9]{2}$/i.test(coords);
     }
+
+    #getRowIndex(coords) {
+        const [rowIndex] = coords;
+
+        return rowIndex;
+    }
+
+    #getColIndex(coords) {
+        const [, colIndex] = coords;
+
+        return colIndex;
+    }
+
+    // 05 - domknięcia (ang. closure)
+    // function getRowIndex(coords) {
+    //     const [rowIndex] = coords;
+
+    //     return rowIndex;
+    // }
+
+    // function getColIndex(coords) {
+    //     const [, colIndex] = coords;
+
+    //     return colIndex;
+    // }
 }

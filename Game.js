18cbe09bb9a13823ce28d8ccd732deff5db5ba2c
Mateusz Bogolubow <mateusz@bogolubow.com>
2023-11-15 07:55:57 +0100

    init 07-04

--- Game.js
+++ Game.js
@@ -1,11 +1,21 @@
+import Board from './Board.js';
+
 export default class Game {
     #board;
     #player1;
     #player2;
 
     constructor(board, player1, player2) {
-        this.#board = board;
+        this.board = board;
         this.#player1 = player1;
         this.#player2 = player2;
+    }
+
+    set board(value) {
+        if (!(value instanceof Board)) {
+            throw new Error('Invalid argument!');
+        }
+
+        this.#board = value;
     }
 }
